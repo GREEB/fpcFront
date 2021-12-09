4e822849e12707029e9a89257387831a35d3dfe7
GREEB <gr44bli@gmail.com>
2021-12-09 18:49:37 +0100

    server moved to new repo

--- server-middleware_old/models/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const UserSchema = new mongoose.Schema({
-  mid: 'number',
-});
-
-const User = mongoose.model('User', UserSchema);
-export default User;
--- server-middleware_old/modules/defaults.js
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-
-const dirname = path.resolve();
-export { path, dirname };
-
-export const age = (obj) => ((Date.now() - obj.lastSeen) / 1000);
